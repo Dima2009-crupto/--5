b41c4e458ab8b5c89fa350816eebe7245deb61db
Dima2009-crupto <dimaparoldima1234@gmail.com>
2024-10-20 12:29:31 +0200

    first commit

---
+++ app/models/employee.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from sqlalchemy import String, Float
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+from app.models.base import Base
+from app.models.position import Position
+from app.models.associates import position_employee_assoc_table
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+    
+    id: Mapped[int] = mapped_column("id", primary_key = True)
+    first_name: Mapped[str] = mapped_column(String(50))
+    last_name: Mapped[str] = mapped_column(String(50))
+    salary: Mapped[Float] = mapped_column(Float())
+    age: Mapped[int] = mapped_column()
+    position: Mapped[List[Position]] = relationship(secondary=position_employee_assoc_table)
+    
+    
+    
